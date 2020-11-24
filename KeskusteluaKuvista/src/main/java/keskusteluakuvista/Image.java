c0a8b9dfbf450d186b798af1c7309f2ea15e884d
kallioaa <aatu.kallio@aalto.fi>
2020-11-24 20:54:04 +0200

    Checkstyle virheiden poistelua

--- KeskusteluaKuvista/src/main/java/keskusteluakuvista/Image.java
+++ KeskusteluaKuvista/src/main/java/keskusteluakuvista/Image.java
@@ -24,11 +24,9 @@ public class Image {
     The for loop takes 100 rgb values from the picture and stores them into a list. From these parametesrs the hashcode is created for the database which for now is just a hashmap
     */
     public Image(BufferedImage image) {
-        
         this.pixels = new ArrayList<>();
         this.height = image.getHeight();
         this.width = image.getWidth();
-        
         for (int x=0;x<=this.width-this.width/10;x=x+this.width/10) {
             for (int y=0;y<=this.height-this.height/10;y=y+this.height/10) {
                 pixels.add(image.getRGB(x, y));
@@ -39,33 +37,30 @@ public class Image {
     @Override
     public boolean equals(Object o) {
         // self check
-        if (this == o)
+        if (this==o)
             return true;
         // null check
-        if (o == null)
+        if (o==null)
             return false;
         // type check and cast
-        if (getClass() != o.getClass())
+        if (getClass()!=o.getClass())
             return false;
-        Image img = (Image) o;
+        Image img=(Image) o;
         if (!Objects.equals(this.height, img.height)) {
             return false;
         }
         if (!Objects.equals(this.width, img.width)) {
             return false;
         }
-        return img.pixels.equals(this.pixels);
-         
+        return img.pixels.equals(this.pixels); 
     }
+    
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 73 * hash + Objects.hashCode(this.height);
-        hash = 73 * hash + Objects.hashCode(this.width);
-        hash = 73 * hash + Objects.hashCode(this.pixels);
+        int hash=7;
+        hash=73 *hash+Objects.hashCode(this.height);
+        hash=73*hash+Objects.hashCode(this.width);
+        hash=73*hash+Objects.hashCode(this.pixels);
         return hash;
     }
-
-   
-    
 }
